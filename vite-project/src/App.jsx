e0720f112ab1bae7f8a80b177dcbfa6d4828d33d
Awais-skylinx <110018365+Awais-skylinx@users.noreply.github.com>
2025-07-13 01:33:25 +0300

    Project Initialized

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from './components/navBar'
+
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  )
+}
+
+export default App
---
+++ vite-project/src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './NavBar.css'
+
+const NavBar = () => {
+  return (
+    <nav>
+        <img src='' />
+        <ul>
+            <li>Home</li>
+            <li>Services</li>
+            <li>How it works?</li>
+            <li>Pricing</li>
+            <li>Contact</li>
+            <li>Submit Request</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default NavBar
